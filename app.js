00195f516e176913a712434d4b75018999d82aa4
[Mark Thomas] <[mark.i.thomas@outlook.com]>
2022-08-17 20:58:50 +0700

    Auth0

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/src/v1/public`));
 app.use(auth(config));
-app.set('trust proxy', 1);
+// app.set('trust proxy', 1);
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
