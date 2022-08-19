f286151b3621a435114dc1480f79eea4819b813f
[Mark Thomas] <[mark.i.thomas@outlook.com]>
2022-08-19 19:07:42 +0700

    Switch Root of Router space to Home Page

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ app.use((req, res, next) => {
 
 // 2) ROUTES
 
-app.use('/api/v1/todos', todoRouter);
+// app.use('/api/v1/todos', todoRouter);
+app.use('/', todoRouter);
 
 module.exports = app;
--- src/v1/routes/todoRoutes.js
+++ src/v1/routes/todoRoutes.js
@@ -5,32 +5,32 @@ const router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
 
 router
-  .route('/')
+  .route('/api/v1/todos')
   .get(requiresAuth(), todoController.getAllTodos)
   .post(requiresAuth(), todoController.createTodo);
 
-router.route('/callback').post(todoController.callback);
+// router.route('/callback').post(todoController.callback);
 
 router
-  .route('/add')
+  .route('/api/v1/todos/add')
   .get(requiresAuth(), todoController.createTodoTemplate)
   .post(requiresAuth(), todoController.createTodo);
 
 router
-  .route('/customers')
+  .route('/api/v1/todos/customers')
   .get(requiresAuth(), customerController.getAllCustomers)
   .post(requiresAuth(), customerController.createCustomer);
 
 router
-  .route('/customers/add')
+  .route('/api/v1/todos/customers/add')
   .get(requiresAuth(), customerController.addCustomer);
 
 router
-  .route('/customers/:id')
+  .route('/api/v1/todos/customers/:id')
   .delete(requiresAuth(), customerController.deleteCustomer);
 
 router
-  .route('/:id')
+  .route('/api/v1/todos/:id')
   .get(requiresAuth(), todoController.getTodo)
   .post(requiresAuth(), todoController.updateTodo)
   .delete(requiresAuth(), todoController.deleteTodo);
