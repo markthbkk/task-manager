51fed9f409574222e9d0259b8ddf24fc95d56bda
[Mark Thomas] <[mark.i.thomas@outlook.com]>
2022-08-30 17:00:57 +0700

    fix DESC new line format in HBS 2

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const hbs = exphbs.create({
   layoutsDir: layoutsDirPath,
 });
 
-hbs.Handlebars.registerHelper('breaklines', function (text) {
+hbs.handlebars.registerHelper('breaklines', function (text) {
   text = Handlebars.Utils.escapeExpression(text);
   text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
   return new Handlebars.SafeString(text);
